0e1f6c8095f0413df82add778734ce1322d7d5b1
ghostcake13 <mirandaseverns@gmail.com>
2023-03-06 22:34:32 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <button classname="btn-primary">Button</button>
+    </div>
+  );
 }
