4db9c01089c6db45687c16933208165516b16477
ghostcake13 <mirandaseverns@gmail.com>
2023-03-09 21:46:33 -0800

    correct icon clearfix, add forecast components

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,19 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
   return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col">day icon 00 00</div>
+        <div className="col-3">
+          <div className="ForecastDay"> day</div>{" "}
+          <WeatherIcon code="01d" size={32} />{" "}
+          <div className="ForecastTemperatures">
+            <span className="ForecastMax">00</span>{" "}
+            <span className="ForecastMin">00</span>
+          </div>
+        </div>
       </div>
     </div>
   );
