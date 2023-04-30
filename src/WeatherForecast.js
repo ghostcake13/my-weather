f36f00ab2b6df1dc9e8ec2dee98e2cfdf3489b49
ghostcake13 <mirandaseverns@gmail.com>
2023-04-30 15:47:09 -0700

    Corrected version and apiKey

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -12,24 +12,25 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
   if (loaded) {
-    console.log(forecast);
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col-3">
-          <div className="ForecastDay"> day</div>{" "}
-          <WeatherIcon code="01d" size={32} />{" "}
-          <div className="ForecastTemperatures">
-            <span className="ForecastMax">{forecast[0].temp.max}</span>{" "}
-            <span className="ForecastMin">{forecast[0].temp.min}</span>
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col-3">
+            <div className="ForecastDay">{forecast[0].dt}y</div>{" "}
+            <WeatherIcon code={forecast[0].weather[0].icon} size={32} />{" "}
+            <div className="ForecastTemperatures">
+              <span className="ForecastMax">{forecast[0].temp.max}</span>{" "}
+              <span className="ForecastMin">{forecast[0].temp.min}</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>;
+    );
   } else {
-    let apiKey = "b4b83d40bb996cdee061104aca316784;";
+    let apiKey = "0efb4fc16a9ed98dc0b3aafd8491d6ad";
     let lat = props.coord.lat;
     let lon = props.coord.lon;
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
 
